535160d18638ec8d6b379ec52dff263164f4e63d
Dominic Canora <dominiccanora@gmail.com>
2016-12-26 10:00:25 -0500

    Verse 1 done

--- src/main/java/net/hazmatrobotics/lightshow/ShowClient.java
+++ src/main/java/net/hazmatrobotics/lightshow/ShowClient.java
@@ -6,15 +6,19 @@ public class ShowClient {
     private WebSocket socket;
     private Integer grade = 0;
     private Integer id;
+    private long lastSendTime;
 
     public void send(String colorString) {
-        if (socket != null)
+        if (socket != null && socket.getReadyState() == WebSocket.READYSTATE.OPEN)
             socket.send(colorString);
+        System.out.println(System.currentTimeMillis() - lastSendTime);
+        lastSendTime = System.currentTimeMillis();
     }
 
     public ShowClient setSocket(WebSocket socket) {
         this.socket = socket;
         this.id = socket.hashCode();
+        this.lastSendTime = System.currentTimeMillis();
         return this;
     }
 
--- src/main/java/net/hazmatrobotics/lightshow/ShowRunner.java
+++ src/main/java/net/hazmatrobotics/lightshow/ShowRunner.java
@@ -8,14 +8,15 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
     private Long startTime;
     private Boolean shouldStop = false;
     private Integer i, j, k;
+    private Float f, g, h;
     private Boolean d;
     private Random r;
 
-    Color c1 = new Color("#4D9DE0");
-    Color c2 = new Color("#E15554");
-    Color c3 = new Color("#E1BC29");
-    Color c4 = new Color("#3BB273");
-    Color c5 = new Color("#7768AE");
+    Color c1 = new Color("#F05500");
+    Color c2 = new Color("#EE2400");
+    Color c3 = new Color("#F4AC00");
+    Color c4 = new Color("#FFDB00");
+    Color c5 = new Color("#FF9A00");
     Thread t;
 
     public ShowRunner(ShowClient client, long startTime, long seed) {
@@ -35,10 +36,14 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
             w(0);
             //System.out.println("Show Start");
             intro();
+            //ohFade1();
+            off(); //TODO: Remove this
             verse1();
         } catch (InterruptedException | RuntimeException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Show End");
     }
 
     private void intro() throws InterruptedException {
@@ -84,13 +89,13 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
         //Victorious
         i = split(4);
         w(10314);
-        if (i == 0) s(255, 141, 0);
+        if (i == 0) s(c1);
         w(10540);
-        if (i == 1) s(255, 231, 0);
+        if (i == 1) s(c2);
         w(11124);
-        if (i == 2) s(255, 180, 0);
+        if (i == 2) s(c3);
         w(11505);
-        if (i == 3) s(255, 0, 0);
+        if (i == 3) s(c4);
         //Oh oh oh oh
         w(11926);
         s(20, 20, 20);
@@ -121,17 +126,24 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
         if (grade == 11) s(255, 0, 0);
         w(17957);
         if (grade == 9) s(255, 0, 0);
+    }
+
+    private void ohFade1() throws InterruptedException {
         //Sustained oh
         i = 255 + split(128);
         d = false;
         w(20415);
+        f = 15f;
         dw(27239, () -> {
             if (d) i++;
             else i--;
             if (i < 128) d = true;
             if (i > 254) d = false;
             s(i, 0, 0);
-            p(5);
+
+            f -= 0.018f;
+            System.out.println(f);
+            p(Math.round(f));
         });
         dw(28590, () -> {
             if (i > 20) i--;
@@ -146,6 +158,7 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
         //Double bubble disco queen
         if (grade == 10) { /* *************************************************************************************** */
             i = 0;
+            off();
             if (split(4) == 0) s(c1);
             w(29400);
             if (split(4) == 0) s(c2);
@@ -161,23 +174,25 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
             if (split(4) == 0) s(c1);
             w(31180);
             off();
+        } else {
+            w(29671);
+            i = 128;
+            dw(30726, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+            i = 128;
+            dw(31302, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
         }
-        w(29671);
-        i = 128;
-        dw(30726, () -> {
-            i--;
-            s(i, i, i);
-            p(10);
-        });
-        i = 128;
-        dw(31302, () -> {
-            i--;
-            s(i, i, i);
-            p(10);
-        });
         //Headed to the guillotine
         if (grade == 11) { /* ************************************************************************************** */
             i = 0;
+            off();
             w(31302);
             if (split(4) == 0) s(c1);
             w(31589);
@@ -194,32 +209,34 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
             if (split(4) == 0) s(c1);
             w(33379);
             off();
+        } else {
+            dw(31859, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+            i = 128;
+            dw(32952, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+            i = 128;
+            dw(33481, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
         }
-        dw(31859, () -> {
-            i--;
-            s(i, i, i);
-            p(10);
-        });
-        i = 128;
-        dw(32952, () -> {
-            i--;
-            s(i, i, i);
-            p(10);
-        });
-        i = 128;
-        dw(33481, () -> {
-            i--;
-            s(i, i, i);
-            p(10);
-        });
         //Skin as cool as Steve McQueen
         if (grade == 9) { /* *************************************************************************************** */
             i = 0;
+            off();
             w(33481);
             if (split(4) == 0) s(c4);
             w(33791);
             if (split(4) == 0) s(c2);
-            w(24042);
+            w(34042);
             if (split(4) == 0) s(c1);
             w(34308);
             if (split(4) == 0) s(c3);
@@ -231,12 +248,164 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
             if (split(4) == 0) s(c3);
             w(35517);
             off();
+        } else {
+            dw(34042, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+            i = 128;
+            dw(35107, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+            i = 128;
+            dw(35669, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
         }
-        dw(34042, () -> {
+        //Let me be your killer king (all grades)
+        /* ********************************************************************************************************* */
+        i = split(7);
+        off();
+        w(35669);
+        if (i == 0) s(c5);
+        w(35956);
+        if (i == 1) s(c4);
+        w(36203);
+        if (i == 2) s(c1);
+        w(36487);
+        if (i == 3) s(c2);
+        w(36740);
+        if (i == 4) s(c3);
+        w(37007);
+        if (i == 5) s(c2);
+        w(37262);
+        if (i == 6) s(c4);
+        w(37634);
+        off();
+/* ********************************************************************************************************************
+ **********************************************************************************************************************
+ ******************************************************************************************************************** */
+        //Hurts until it stops
+        if (grade == 10) { /* *************************************************************************************** */
+            i = 0;
+            off();
+            w(37854);
+            if (split(4) == 0) s(c1);
+            w(38150);
+            if (split(4) == 0) s(c2);
+            w(38388);
+            if (split(4) == 0) s(c3);
+            w(38702);
+            if (split(4) == 0) s(c4);
+            w(38818);
+            if (split(4) == 0) s(c5);
+            w(39233);
+            off();
+        } else {
+            w(38388);
+            i = 128;
+            dw(39497, () -> {
+                i--;
+                s(i, i, i);
+                p(10);
+            });
+        }
+        //We will love until it's not
+        if (grade == 11) { /* ************************************************************************************** */
+            i = 0;
+            off();
+            w(39497);
+            if (split(4) == 0) s(c1);
+            w(39758);
+            if (split(4) == 0) s(c4);
+            w(40072);
+            if (split(4) == 0) s(c3);
+            w(40316);
+            if (split(4) == 0) s(c2);
+            w(40580);
+            if (split(4) == 0) s(c5);
+            w(40867);
+            if (split(4) == 0) s(c2);
+            w(41099);
+            if (split(4) == 0) s(c1);
+            w(41401);
+            off();
+        }
+        dw(40580, () -> {
             i--;
             s(i, i, i);
             p(10);
         });
+        i = 128;
+        dw(41671, () -> {
+            i--;
+            s(i, i, i);
+            p(10);
+        });
+        //I'm a killing spree in white
+        if (grade == 9) { /* *************************************************************************************** */
+            i = 0;
+            off();
+            w(41671);
+            if (split(4) == 0) s(c4);
+            w(42025);
+            if (split(4) == 0) s(c2);
+            w(42217);
+            if (split(4) == 0) s(c1);
+            w(42472);
+            if (split(4) == 0) s(c3);
+            w(42643);
+            if (split(4) == 0) s(c5);
+            w(43000);
+            if (split(4) == 0) s(c4);
+            w(43230);
+            if (split(4) == 0) s(c3);
+            w(43607);
+            off();
+        }
+        i = 128;
+        dw(42643, () -> {
+            i--;
+            s(i, i, i);
+            p(10);
+        });
+        i = 128;
+        dw(43828, () -> {
+            i--;
+            s(i, i, i);
+            p(10);
+        });
+        //Ahh
+        /* ********************************************************************************************************* */
+        w(43828);
+        s(255, 255, 255);
+        w(44368);
+        off();
+        //Eyes like broken christmas lights (all grades)
+        /* ********************************************************************************************************* */
+        i = split(7);
+        off();
+        w(44382);
+        if (i == 0) s(c5);
+        w(44678);
+        if (i == 1) s(c4);
+        w(44934);
+        if (i == 2) s(c1);
+        w(45212);
+        if (i == 3) s(c2);
+        w(45430);
+        if (i == 4) s(c3);
+        w(45755);
+        if (i == 5) s(c2);
+        w(46031);
+        if (i == 6) s(c4);
+        w(46277);
+        off();
     }
 
     private void s(Integer r, Integer g, Integer b) {
@@ -253,6 +422,7 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
     }
 
     private long z() { //Current show time
+        if (shouldStop) return Long.MAX_VALUE;
         return System.currentTimeMillis() - startTime;
     }
 
@@ -266,8 +436,8 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            System.out.println("Interrupted.");
+            shouldStop = true;
         }
     }
 
@@ -294,6 +464,5 @@ public class ShowRunner implements Runnable { //TODO: More gracefully handle sho
 
     public void stop() {
         shouldStop = true;
-        t.interrupt();
     }
 }
