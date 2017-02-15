b3b65ce421c15d97f504d94421c4122612e47f9f
Dominic Canora <dominiccanora@gmail.com>
2017-02-15 15:50:29 -0500

    Duck DNS

--- src/main/java/net/hazmatrobotics/lightshow/coordinators/MainCoordinator.java
+++ src/main/java/net/hazmatrobotics/lightshow/coordinators/MainCoordinator.java
@@ -1,6 +1,10 @@
 package net.hazmatrobotics.lightshow.coordinators;
 
+import net.hazmatrobotics.lightshow.Secret;
 import net.hazmatrobotics.lightshow.ShowServer;
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class MainCoordinator {
     private static ShowServer server;
@@ -9,6 +13,8 @@ public class MainCoordinator {
     public static final Integer LOAD_TEST_AMNT = 1000;
 
     public static void main(String... args) throws InterruptedException {
+        updateDNS();
+
         server = new ShowServer(8000);
         server.start();
 
@@ -38,5 +44,33 @@ public class MainCoordinator {
 
     public static void cleanup() {
         server.cleanup();
+    }
+
+    private static void updateDNS() {
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("www.duckdns.org")
+                .addPathSegment("update")
+                .addQueryParameter("domains", Secret.duckDomain)
+                .addQueryParameter("token", Secret.duckKey)
+                .addQueryParameter("verbose", "true")
+                .build();
+
+        Request r = new Request.Builder()
+                .url(url)
+                .build();
+
+        new OkHttpClient().newCall(r).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.print("Updated DNS: ");
+                System.out.println(response.body().string().replace("\n", " "));
+            }
+        });
     }
 }
